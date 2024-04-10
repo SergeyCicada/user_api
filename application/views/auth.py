b11acbc8adedf6f79f1c61633177e0d2d267412c
sergeycicada <SergeyKelme@yandex.ru>
2024-04-11 03:10:37 +0400

    added_refresh_token

--- application/views/auth.py
+++ application/views/auth.py
@@ -20,3 +20,11 @@ class AuthView(Resource):
         tokens = auth_service.generate_tokens(username, password)
 
         return tokens, 201
+
+    def put(self):
+        data = request.json
+        token = data.get("refresh_token")
+
+        tokens = auth_service.approve_refresh_token(token)
+
+        return tokens, 201
